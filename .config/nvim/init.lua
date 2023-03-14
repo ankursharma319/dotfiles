eda8602e5052315cd3ba9ceac43597774856cdf9
Ankur Sharma <ankurs4@cisco.com>
2023-03-14 10:31:21 +0100

    nvim: remove tab keymap in cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -818,25 +818,8 @@ cmp.setup({
         },
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-
+        ['<Tab>'] = nil,
+        ['<S-Tab>'] = nil,
         -- -- go to next placeholder in the snippet
         -- ['<C-f>'] = cmp.mapping(function(fallback)
         --     if luasnip.jumpable(1) then
