efa2fee3071e6bf71251d444a5fe992827e22a0c
Ankur Sharma <ankurs4@cisco.com>
2023-03-19 17:18:36 +0100

    nvim: add telescope livegrep picker with glob args

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -618,6 +618,69 @@ local wrap = function(func, ...)
     end
 end
 
+local live_grep_with_args = function(opts)
+    local conf = require("telescope.config").values
+    local finders = require "telescope.finders"
+    local make_entry = require "telescope.make_entry"
+    local pickers = require "telescope.pickers"
+    local flatten = vim.tbl_flatten
+    opts = opts or {}
+    opts.cwd = opts.cwd and vim.fn.expand(opts.cwd) or vim.loop.cwd()
+    opts.shortcuts = opts.shortcuts
+        or {
+                ["l"] = "*.lua",
+                ["v"] = "*.vim",
+                ["n"] = "*.{vim,lua}",
+                ["c"] = "*.c",
+        }
+    opts.pattern = opts.pattern or "%s"
+
+    local custom_grep = finders.new_async_job {
+        command_generator = function(prompt)
+            if not prompt or prompt == "" then
+                return nil
+            end
+
+            local prompt_split = vim.split(prompt, "  ", { trimempty = true })
+
+            local args = { "rg" }
+            if prompt_split[1] then
+                table.insert(args, "-e")
+                table.insert(args, prompt_split[1])
+                table.remove(prompt_split, 1)
+            end
+            ---@diagnostic disable-next-line: unused-local
+            for i, arg in ipairs(prompt_split) do
+                table.insert(args, "--iglob")
+
+                local pattern
+                if opts.shortcuts[arg] then
+                    pattern = opts.shortcuts[arg]
+                else
+                    pattern = arg
+                end
+
+                table.insert(args, string.format(opts.pattern, pattern))
+            end
+
+            return flatten {
+                args,
+                { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+            }
+        end,
+        entry_maker = make_entry.gen_from_vimgrep(opts),
+        cwd = opts.cwd,
+    }
+
+    pickers.new(opts, {
+        debounce = 100,
+        prompt_title = "Live Grep (with shortcuts)",
+        finder = custom_grep,
+        previewer = conf.grep_previewer(opts),
+        sorter = require("telescope.sorters").empty(),
+    }):find()
+end
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
 vim.keymap.set('n', '<leader>fa', wrap(builtin.find_files, { hidden = true }),
@@ -626,6 +689,7 @@ vim.keymap.set('n', '<leader>fF', builtin.git_files, { desc = '[F]ind [G]it [F]i
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>fG', live_grep_with_args, { desc = '[S]earch by [G]rep with glob args' })
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
 
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
